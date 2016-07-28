d7888516b990e6c86458e559995f1d75a1578eab
Patrick <patrick.mell@stud.tu-darmstadt.de>
2016-07-28 22:57:58 +0200

    Added the City ORM model as well as the DatabaseHelper to make use of the DAO.

---
+++ app/src/main/java/org/secuso/privacyfriendlyweather/orm/DatabaseHelper.java
@@ -0,0 +1,106 @@
+package org.secuso.privacyfriendlyweather.orm;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import org.secuso.privacyfriendlyweather.R;
+import org.secuso.privacyfriendlyweather.files.FileReader;
+import org.secuso.privacyfriendlyweather.pojos.City;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by ptrck on 28/07/16.
+ */
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+
+    /**
+     * Database information
+     */
+    private static final String DATABASE_NAME = "privacyfriendlyweather";
+    private static final int DATABASE_VERSION = 1;
+
+    private Context context;
+
+    /**
+     * The data access object used to interact with the Sqlite database to do C.R.U.D operations.
+     */
+    private Dao<City, Integer> cityDao;
+
+    /**
+     * @see OrmLiteSqliteOpenHelper
+     */
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    private void fillCitiesTable() {
+        Log.d("debug_info", "Fill DB");
+        InputStream inputStream = context.getResources().openRawResource(R.raw.city_list);
+        FileReader fileReader = new FileReader();
+        try {
+            List<City> cities = fileReader.readCitiesFromFile(inputStream);
+            inputStream.close();
+            for (City city : cities) {
+                cityDao.create(city);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see OrmLiteSqliteOpenHelper#onCreate(SQLiteDatabase, ConnectionSource)
+     */
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+        try {
+            TableUtils.createTable(connectionSource, City.class);
+            fillCitiesTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see OrmLiteSqliteOpenHelper#onUpgrade(SQLiteDatabase, int, int)
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+            TableUtils.dropTable(connectionSource, City.class, false);
+            onCreate(database, connectionSource);
+            fillCitiesTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @return Returns an instance of the DAO.
+     */
+    public Dao<City, Integer> getDao() {
+        if (cityDao == null) {
+            try {
+                cityDao = getDao(City.class);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return cityDao;
+    }
+
+}
---
+++ app/src/main/java/org/secuso/privacyfriendlyweather/pojos/City.java
@@ -0,0 +1,123 @@
+package org.secuso.privacyfriendlyweather.pojos;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * This plain old Java class is the database model for cities.
+ */
+@DatabaseTable(tableName = "cities")
+public class City {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField(columnName = "city_id")
+    private int cityId;
+
+    @DatabaseField(columnName = "city_name")
+    private String cityName;
+
+    @DatabaseField(columnName = "latitude")
+    private double lat;
+
+    @DatabaseField(columnName = "longitude")
+    private double lon;
+
+    @DatabaseField(columnName = "country_code")
+    private String countryCode;
+
+    /**
+     * Constructor.
+     */
+    public City() {
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param cityId      The ID of the city.
+     * @param cityName    The name of the city.
+     * @param countryCode The code of the country that the city belongs to.
+     * @param lat         Latitude.
+     * @param lon         Longitude.
+     */
+    public City(int cityId, String cityName, String countryCode, double lat, double lon) {
+        this.cityId = cityId;
+        this.cityName = cityName;
+        this.countryCode = countryCode;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    /**
+     * @return Returns the ID of the city.
+     */
+    public int getCityId() {
+        return cityId;
+    }
+
+    /**
+     * @param cityId The ID of the city.
+     */
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    /**
+     * @return Returns the name of the city.
+     */
+    public String getCityName() {
+        return cityName;
+    }
+
+    /**
+     * @param cityName The name of the city to set.
+     */
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    /**
+     * @return Returns the latitude.
+     */
+    public double getLat() {
+        return lat;
+    }
+
+    /**
+     * @param lat The latitude of the city to set.
+     */
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    /**
+     * @return Returns the longitude.
+     */
+    public double getLon() {
+        return lon;
+    }
+
+    /**
+     * @param lon The longitude of the city to set.
+     */
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    /**
+     * @return Returns the country code that the city belongs to.
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    /**
+     * @param countryCode The country code that the city belongs to.
+     */
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+}
