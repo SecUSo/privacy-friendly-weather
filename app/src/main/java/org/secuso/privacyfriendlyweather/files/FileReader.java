77e2902e41852062de8b5e3d224ec7b796dd0e5c
Patrick <patrick.mell@stud.tu-darmstadt.de>
2016-07-28 22:59:12 +0200

    Necessary files for importing the city_list.txt file were added.

---
+++ app/src/main/java/org/secuso/privacyfriendlyweather/files/FileReader.java
@@ -0,0 +1,45 @@
+package org.secuso.privacyfriendlyweather.files;
+
+import org.secuso.privacyfriendlyweather.pojos.City;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Provides functionality for reading files.
+ */
+public class FileReader {
+
+    /**
+     *
+     * @param is
+     * @return
+     * @throws IOException
+     */
+    public List<City> readCitiesFromFile(InputStream is) throws IOException {
+        List<City> cities = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        // Skip the first line as it contains headings
+        String line = br.readLine();
+        while ((line = br.readLine()) != null) {
+            // id, name, lat, lon, country code
+            String[] fields = line.split("\t");
+
+            int id = Integer.parseInt(fields[0]);
+            String name = fields[1];
+            double lat = Double.parseDouble(fields[2]);
+            double lon = Double.parseDouble(fields[3]);
+            String countryCode = fields[4];
+            cities.add(new City(id, name, countryCode, lat, lon));
+        }
+        br.close();
+        return cities;
+    }
+
+}
