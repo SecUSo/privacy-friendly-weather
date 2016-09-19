a3d92957938b91021066849c77fca069c3439a9b
Patrick <patrick.mell@stud.tu-darmstadt.de>
2016-09-19 23:07:10 +0200

    Made the radius search more robust by providing the option to ommit the metric parameter for the HTTP request.

--- app/src/main/java/org/secuso/privacyfriendlyweather/weather_api/open_weather_map/OwmHttpRequest.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/weather_api/open_weather_map/OwmHttpRequest.java
@@ -46,14 +46,16 @@ public class OwmHttpRequest {
      * Builds the URL for the OpenWeatherMap API that can be used to query the weather for a single
      * city.
      *
-     * @param cityId The ID of the city to get the data for.
+     * @param cityId    The ID of the city to get the data for.
+     * @param useMetric If set to true, temperature values will be in Celsius.
      * @return Returns the URL that can be used to query the weather for the given city.
      */
-    protected String getUrlForQueryingSingleCity(int cityId) {
+    protected String getUrlForQueryingSingleCity(int cityId, boolean useMetric) {
         return String.format(
-                "%sweather?id=%s&units=metric&appid=%s",
+                "%sweather?id=%s%s&appid=%s",
                 OwmApiData.BASE_URL,
                 cityId,
+                (useMetric) ? "&units=metric" : "",
                 OwmApiData.API_KEY
         );
     }
--- app/src/main/java/org/secuso/privacyfriendlyweather/weather_api/open_weather_map/OwmHttpRequestForRadiusSearch.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/weather_api/open_weather_map/OwmHttpRequestForRadiusSearch.java
@@ -30,7 +30,7 @@ public class OwmHttpRequestForRadiusSearch extends OwmHttpRequest implements IHt
     public void perform(int cityId, int edgeLength, int resultCount) {
         // First, retrieve the city from OWM to have the latitude and longitude for the city
         IHttpRequest httpRequest = new VolleyHttpRequest(context);
-        final String URL = getUrlForQueryingSingleCity(cityId);
+        final String URL = getUrlForQueryingSingleCity(cityId, false);
         httpRequest.make(URL, HttpRequestType.GET, new ProcessRadiusSearchRequest(context, edgeLength, resultCount));
     }
 
