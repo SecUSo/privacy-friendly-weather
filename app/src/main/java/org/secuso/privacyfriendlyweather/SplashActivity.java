b52a2e1ff77e5afd752a6564e90a3375ef54f669
Karola Marky <yonjuni@pinyto.de>
2016-10-24 22:19:47 +0300

    Placeholder Splash Screen added

--- app/src/main/java/org/secuso/privacyfriendlyweather/SplashActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/SplashActivity.java
@@ -1,5 +1,7 @@
 package org.secuso.privacyfriendlyweather;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,6 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+        SplashActivity.this.startActivity(mainIntent);
+        SplashActivity.this.finish();
 
+    }
 
 }
