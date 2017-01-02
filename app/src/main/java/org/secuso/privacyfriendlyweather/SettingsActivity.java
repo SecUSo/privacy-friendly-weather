1aa69ee19e3e44b79981ea4670aff95727d5038d
Karola <yonjuni@pinyto.de>
2017-01-02 02:34:05 +0200

    Structure for needed settings added

--- app/src/main/java/org/secuso/privacyfriendlyweather/SettingsActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/SettingsActivity.java
@@ -91,9 +91,6 @@ public class SettingsActivity extends BaseActivity {
 
         setContentView(R.layout.activity_settings);
 
-        //setupActionBar();
-
-
         overridePendingTransition(0, 0);
     }
 
@@ -168,14 +165,11 @@ public class SettingsActivity extends BaseActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
-            //setHasOptionsMenu(true);
 
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-            //bindPreferenceSummaryToValue(findPreference("example_text"));
-            //bindPreferenceSummaryToValue(findPreference("example_list"));
+            bindPreferenceSummaryToValue(findPreference("API_key_value"));
+
+            //TODO Handle entering and reseting of API keys
+
         }
 
         @Override
