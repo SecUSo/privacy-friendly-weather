a198800bb1696925d612b1247d87ddaf97b46e57
Karola <yonjuni@pinyto.de>
2017-01-02 18:25:14 +0200

    set/reset API keys

--- app/src/main/java/org/secuso/privacyfriendlyweather/weather_api/open_weather_map/OwmApiData.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/weather_api/open_weather_map/OwmApiData.java
@@ -6,12 +6,22 @@ package org.secuso.privacyfriendlyweather.weather_api.open_weather_map;
 public class OwmApiData {
 
     public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
-    public static final String API_KEY = "3fb239a5397459ff57d21c5cc1ca1536";
+    public static String API_KEY = "3fb239a5397459ff57d21c5cc1ca1536";
+    public static String DEFAULT_API_KEY = "3fb239a5397459ff57d21c5cc1ca1536";
 
     /**
      * Make this class static.
      */
     private OwmApiData() {
+    }
+
+    //TODO Handling of set/reseting API Keys in the Settings
+    public void setAPI_KEY(String API_KEY) {
+        this.API_KEY = API_KEY;
+    }
+
+    public void resetAPI_KEY() {
+        this.API_KEY = DEFAULT_API_KEY;
     }
 
 }
