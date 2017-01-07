a58fdb6beb8dc9d08305e1869b48fa4fd8d99c2e
Karola <yonjuni@pinyto.de>
2017-01-07 02:11:47 +0200

    Refresh button added

--- app/src/main/java/org/secuso/privacyfriendlyweather/activities/ForecastCityActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/activities/ForecastCityActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import org.secuso.privacyfriendlyweather.preferences.PrefManager;
 import org.secuso.privacyfriendlyweather.R;
@@ -76,6 +78,26 @@ public class ForecastCityActivity extends BaseActivity {
         prefManager.setFirstTimeLaunch(false);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_forecast_city, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.menu_refresh) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
