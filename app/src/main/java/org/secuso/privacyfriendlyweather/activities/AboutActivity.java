6ad22e78a67ba88b8bcd2f06f75a69051904a116
Thomas Glaser <thomas.glaser@stud.tu-darmstadt.de>
2017-05-31 09:18:05 +0200

    Navigation Drawer zur About-Page hinzufügen

--- app/src/main/java/org/secuso/privacyfriendlyweather/activities/AboutActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/activities/AboutActivity.java
@@ -13,33 +13,34 @@ import org.secuso.privacyfriendlyweather.R;
 /**
  * Created by yonjuni on 15.06.16.
  */
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends BaseActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        overridePendingTransition(0, 0);
 
-        ActionBar ab = getSupportActionBar();
-        if(ab != null) {
-            ab.setDisplayHomeAsUpEnabled(true);
-        }
 
-        View mainContent = findViewById(R.id.main_content);
-        if (mainContent != null) {
-            mainContent.setAlpha(0);
-            mainContent.animate().alpha(1).setDuration(BaseActivity.MAIN_CONTENT_FADEIN_DURATION);
-        }
+        //ActionBar ab = getSupportActionBar();
+        //if(ab != null) {
+        //    ab.setDisplayHomeAsUpEnabled(true);
+        //}
+
+        //View mainContent = findViewById(R.id.main_content);
+        //if (mainContent != null) {
+        //    mainContent.setAlpha(0);
+        //    mainContent.animate().alpha(1).setDuration(BaseActivity.MAIN_CONTENT_FADEIN_DURATION);
+        //}
 
         ((TextView)findViewById(R.id.secusoWebsite)).setMovementMethod(LinkMovementMethod.getInstance());
         ((TextView)findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
         ((TextView)findViewById(R.id.textFieldVersionName)).setText(BuildConfig.VERSION_NAME);
 
-        overridePendingTransition(0, 0);
     }
 
-    //@Override
-    //protected int getNavigationDrawerID() {
-    //    return R.id.nav_about;
-    //}
+    @Override
+    protected int getNavigationDrawerID() {
+        return R.id.nav_about;
+    }
 }
 
