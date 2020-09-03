4e0a4d4bad66e8d44e65105ef015984f86af31c4
sleep-yearning <emailfurunity@web.de>
2020-09-03 13:45:42 +0200

    current weather details are updated properly on update and city adding

--- app/src/main/java/org/secuso/privacyfriendlyweather/weather_api/open_weather_map/ProcessOwmUpdateCityListRequest.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/weather_api/open_weather_map/ProcessOwmUpdateCityListRequest.java
@@ -15,6 +15,7 @@ import org.json.JSONObject;
 import org.secuso.privacyfriendlyweather.R;
 import org.secuso.privacyfriendlyweather.database.CurrentWeatherData;
 import org.secuso.privacyfriendlyweather.database.PFASQLiteHelper;
+import org.secuso.privacyfriendlyweather.ui.updater.ViewUpdater;
 import org.secuso.privacyfriendlyweather.weather_api.IDataExtractor;
 import org.secuso.privacyfriendlyweather.weather_api.IProcessHttpRequest;
 import org.secuso.privacyfriendlyweather.widget.WeatherWidget;
@@ -81,6 +82,8 @@ public class ProcessOwmUpdateCityListRequest implements IProcessHttpRequest {
                     } else {
                         dbHelper.addCurrentWeather(weatherData);
                     }
+                    ViewUpdater.updateCurrentWeatherData(weatherData);
+
                 }
             }
 
