e7c1a9418fc661700d67a1c90c808fb3457ea630
Christopher Beckmann <chris.beckm@gmail.com>
2020-09-03 13:48:33 +0200

    Added TestSuites for androidTest and regular JUnit tests

---
+++ app/src/androidTest/java/org/secuso/privacyfriendlyweather/AndroidTestSuite.java
@@ -0,0 +1,11 @@
+package org.secuso.privacyfriendlyweather;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.secuso.privacyfriendlyweather.database.DatabaseTest;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({DatabaseTest.class})
+public class AndroidTestSuite {
+}
