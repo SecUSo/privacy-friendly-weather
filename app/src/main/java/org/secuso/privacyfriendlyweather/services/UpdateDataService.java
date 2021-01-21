8ee067791eb7822eae82147a6f5fb8c741dc992e
sleep-yearning <emailfurunity@web.de>
2021-01-21 03:06:05 +0100

    limited onecallapi calls to 10 per day for shared key users

--- app/src/main/java/org/secuso/privacyfriendlyweather/services/UpdateDataService.java
+++ app/src/main/java/org/secuso/privacyfriendlyweather/services/UpdateDataService.java
@@ -1,6 +1,7 @@
 package org.secuso.privacyfriendlyweather.services;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
@@ -15,6 +16,7 @@ import org.secuso.privacyfriendlyweather.database.AppDatabase;
 import org.secuso.privacyfriendlyweather.database.data.City;
 import org.secuso.privacyfriendlyweather.database.data.CityToWatch;
 import org.secuso.privacyfriendlyweather.database.data.Forecast;
+import org.secuso.privacyfriendlyweather.preferences.AppPreferencesManager;
 import org.secuso.privacyfriendlyweather.weather_api.IHttpRequestForForecast;
 import org.secuso.privacyfriendlyweather.weather_api.IHttpRequestForOneCallAPI;
 import org.secuso.privacyfriendlyweather.weather_api.open_weather_map.OwmHttpRequestForForecast;
@@ -134,10 +136,43 @@ public class UpdateDataService extends JobIntentService {
                 IHttpRequestForForecast forecastRequest = new OwmHttpRequestForForecast(getApplicationContext());
                 forecastRequest.perform(cityId);
             }
-            IHttpRequestForOneCallAPI forecastOneCallRequest = new OwmHttpRequestForOneCallAPI(getApplicationContext());
-            forecastOneCallRequest.perform(lat, lon);
+            if (oneCallAllowed(getApplicationContext())) {
+                IHttpRequestForOneCallAPI forecastOneCallRequest = new OwmHttpRequestForOneCallAPI(getApplicationContext());
+                forecastOneCallRequest.perform(lat, lon);
+            }
+            //TODO add alternative retrieval with CurrentWeatherData API
+
         }
     }
+
+    private boolean oneCallAllowed(Context context) {
+        AppPreferencesManager appPreferences = new AppPreferencesManager(PreferenceManager.getDefaultSharedPreferences(context));
+        if (appPreferences.usingPersonalKey(context)) {
+            return true;
+        }
+        //if shared key is used, execute limiting code
+        SharedPreferences.Editor editor = prefManager.edit();
+        int currentCalls = prefManager.getInt("shared_calls_used", 0);
+        if (currentCalls == 0) {
+            editor.putLong("shared_calls_count_start", System.currentTimeMillis());
+        }
+        if (currentCalls < 10) {
+            editor.putInt("shared_calls_used", currentCalls + 1);
+            editor.commit();
+            return true;
+            // if calls reached but day since first call elapsed
+        } else if (prefManager.getLong("shared_calls_count_start", 0) + 86400000 < System.currentTimeMillis()) {
+            prefManager.edit().putInt("shared_calls_used", 1);
+            prefManager.edit().putLong("shared_calls_count_start", System.currentTimeMillis());
+            prefManager.edit().commit();
+            return true;
+            // if 10 calls in the last 24 hours used
+        } else {
+            return false;
+            //TODO visualize Toast and stop refreshbutton animation
+        }
+    }
+
 
     private boolean isOnline() {
         try {
